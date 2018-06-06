92e78d899a1ccadee3558b2545d953bbff3c98a3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-06-06 00:44:23 -0300

    Added code sample.

--- samples/react/sample.js
+++ samples/react/sample.js
@@ -1,1 +1,20 @@
-const Foo = () => <div>Content</div>
+import React from 'react'
+
+// Single component.
+export const Single = () => <div>Content</div>
+
+// Nested component.
+export const Nested = () => (
+  <div>
+    <p>Opa</p>
+  </div>
+)
+
+// Attributes.
+export const Attributes = () => (
+  <Nested string='string' object={ { prop: 'value' } } array={ [1, 2, 3] } />
+)
+
+// Children expression.
+// eslint-disable-next-line react/prop-types
+export const Expression = ({ children }) => <div>{ children }</div>
